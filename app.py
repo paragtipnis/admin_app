d79f9fd4cebc389776a765d65476bed792ef569b
paragtipnis <paragatipnis@gmail.com>
2025-04-08 23:25:51 +0530

    Mod 2

--- app.py
+++ app.py
@@ -139,6 +139,6 @@ if st.session_state.df is not None:
         st.info("Click 'Next' to begin navigating the list.")
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 10000))
+#    app.run(host="0.0.0.0", port=port)
